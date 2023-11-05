5559377e7489d63985a23b09d1389c6f33ec8e1e
Evgenij Ivankin <e.ivankin@innopolis.university>
2023-11-05 13:55:42 +0300

    fix formatting and add docstring

--- src/data/download_paranmt.py
+++ src/data/download_paranmt.py
@@ -1,3 +1,8 @@
+"""
+This script downloads the filtered ParaNMT-detox corpus (500K sentence pairs)
+into the `./data/raw` directory and unpacks it without keeping the source archive.
+"""
+
 import os
 import zipfile
 
@@ -8,7 +13,7 @@ from src.config import RAW_DATA_DIR
 # From task description
 DOWNLOAD_URL = "https://github.com/skoltech-nlp/detox/releases/download/emnlp2021/filtered_paranmt.zip"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     file_name = wget.download(DOWNLOAD_URL, out=str(RAW_DATA_DIR))
 
     with zipfile.ZipFile(file_name, "r") as zip_ref:
